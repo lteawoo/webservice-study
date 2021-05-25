10de96e034ff8c725d57b611e935f690d9c371a7
twlee <lteawoo@naver.com>
2021-05-25 21:09:59 +0900

    Web Layer, Service Layer

---
+++ src/main/java/kr/taeu/webservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package kr.taeu.webservice.service.posts;
+
+import kr.taeu.webservice.web.dto.PostsSaveRequestDto;
+import kr.taeu.webservice.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/kr/taeu/webservice/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package kr.taeu.webservice.web;
+
+import kr.taeu.webservice.service.posts.PostsService;
+import kr.taeu.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
