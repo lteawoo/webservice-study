1ab54e18dc583a4c03898d3b7d880d8c1a62e6df
twlee <lteawoo@naver.com>
2021-06-22 21:40:21 +0900

    update

--- src/main/java/kr/taeu/webservice/web/PostsApiController.java
+++ src/main/java/kr/taeu/webservice/web/PostsApiController.java
@@ -1,9 +1,14 @@
 package kr.taeu.webservice.web;
 
 import kr.taeu.webservice.service.posts.PostsService;
+import kr.taeu.webservice.web.dto.PostsResponseDto;
 import kr.taeu.webservice.web.dto.PostsSaveRequestDto;
+import kr.taeu.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
